cbc33e02bbc721c5ab467d9552f3ed38f0653776
BigDog <maxwell.morris@protonmail.com>
2020-03-21 17:37:47 +0800

    Started questions 2 passing by reference

---
+++ Pracs/Prac3/Question_2/order.c
@@ -0,0 +1,17 @@
+
+
+static void  ascending2(*int x, *int y){
+    if ((*x)>(*y)){
+    int original_x = *x;
+    int original_y = *y;
+
+    int *x = original_y;
+    int *y = original_x;
+    }
+}
+
+
+
+
+
+
